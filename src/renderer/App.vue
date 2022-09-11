1cf4ba0b8ebec0d05521fe1674946de3d0b7f0e6
Platina <kkgordon.com@gmail.com>
2022-09-12 00:12:48 +0900

    Added Icons

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,43 @@
 <script setup lang="ts">
-import Hello from './components/Hello.vue'
-import { ipcRenderer } from './electron'
+import Hello from "./components/Hello.vue";
+import { ipcRenderer } from "./electron";
 
-ipcRenderer.send('message', 'Hello from App.vue!');
+// ipcRenderer.send("message", "Hello from App.vue!");
 </script>
 
 <template>
   <div id="app">
-    <Hello/>
+    <Hello />
   </div>
 </template>
+
+<style>
+@import url("https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css");
+@import url("https://fonts.googleapis.com/css?family=Poppins");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", "Apple SD Gothic Neo", "Spoqa Han Sans",
+    "SpoqaHanSans", "Noto Sans KR", "Noto Sans", "Noto Sans CJK KR",
+    "NanumBarunGothic", "Nanum Gothic", "KoPub Dotum", "Malgun Gothic",
+    "맑은 고딕", sans-serif;
+  -webkit-user-select: none;
+}
+
+/* Dark, Light Mode */
+@media (prefers-color-scheme: dark) {
+  body {
+    background: #2c313c;
+    color: white;
+  }
+}
+
+@media (prefers-color-scheme: light) {
+  body {
+    background: #ddd;
+    color: black;
+  }
+}
+</style>
