61a92e90f25eeb615530311f55185bc73d717cb7
Platina <kkgordon.com@gmail.com>
2022-09-15 10:19:07 +0900

    Bitmap Store Import Application

---
+++ src/renderer/components/StoreApp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="store_app">
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfJ3LJQ-ru-_vyI4fgsw8hWjiUQp1Vq9XQ0IV6u_mleuKHnHQ/viewform?embedded=true"
+      width="640"
+      height="3512"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      >로드 중…</iframe
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="css">
+.store_app {
+  margin-top: 36px;
+}
+</style>
