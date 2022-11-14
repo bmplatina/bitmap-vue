ff67540209b78944a00017b3d9e9afd64f2367c2
Platina <kkgordon.com@gmail.com>
2022-11-14 17:13:52 +0900

    Internationalization Settings

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -1,7 +1,41 @@
 <template>
   <div>Settings View</div>
+  <button
+    @click="
+      $i18n.locale = 'ko';
+      currentLanguage = 'ko';
+      if (initialLanguage != currentLanguage) {
+        isRestartRequiredSettingsChanged = true;
+      }
+    "
+  >
+    한국어 (Korean)
+  </button>
+  <button
+    @click="
+      $i18n.locale = 'en';
+      currentLanguage = 'en';
+      if (initialLanguage != currentLanguage) {
+        isRestartRequiredSettingsChanged = true;
+      }
+    "
+  >
+    English
+  </button>
+  <h3 v-show="isRestartRequiredSettingsChanged">{{ $t("requireRestart") }}</h3>
 </template>
 <script defer lang="ts">
-export default {};
+export default {
+  data() {
+    return {
+      initialLanguage: this.$i18n.locale,
+      currentLanguage: "ko",
+      isRestartRequiredSettingsChanged: false,
+    };
+  },
+  created() {},
+  methods: {},
+  computed: {},
+};
 </script>
 <style lang="css"></style>
