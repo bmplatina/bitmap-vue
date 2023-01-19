04e58d7a834d10bfe362ce7ba90ba55585ef3650
Platina <kkgordon.com@gmail.com>
2023-01-19 13:54:13 +0900

    게임 페이지 재생성

---
+++ src/renderer/components/GameInfo.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps<{
+  gameIndex: Number;
+}>();
+</script>
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { gameAPI, ipcRenderer } from "../electron";
+
+export default {
+  data() {
+    return {
+      games: [
+        {
+          gameTitle: "",
+          gameVersion: 1.0,
+          gamePlatformWindows: false,
+          gamePlatformMac: false,
+          gamePlatformMobile: false,
+          gameEngine: "",
+          gameGenre: "",
+          gameDeveloper: "",
+          gamePublisher: "",
+          isEarlyAccess: false,
+          isReleased: false,
+          gameReleasedDate: "",
+          gameWebsite: "",
+          gameVideoURL: "",
+          gameDownloadMacURL: "",
+          gameDownloadWinURL: "",
+          gameLogoURL: "",
+          gameBannerURL: "",
+          gamePosterURL: "",
+          gameBinaryName: "",
+          gameHeadline: "",
+          gameDescription: "",
+        },
+      ],
+    };
+  },
+  created() {
+    gameAPI()
+      .then((response) => (this.games = response.data.games))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+<style scoped></style>
